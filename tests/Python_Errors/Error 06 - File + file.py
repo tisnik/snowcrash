0ee037bc93c1ed3ddf7c6e465a49dc9fdd276533
AlbatrosCZ <dan-pavlik@seznam.cz>
2019-09-04 15:21:52 +0200

    rename and edit Error files

--- tests/Python_Errors/Error 06 - File + file.py
+++
@@ -1,2 +0,0 @@
-file = open("wrong print.py",'a')
-file = file+file # unsupported operand type(s) for +: '_io.TextIOWrapper' and '_io.TextIOWrapper'
---
+++ tests/Python_Errors/io.UnusupportedOperation.py
@@ -0,0 +1,2 @@
+file = open("TypeError.py",'r')
+file.write(" ") # not writable
