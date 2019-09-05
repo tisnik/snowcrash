c1ec1c87f01ee4ad41cec1d33769c3aacec1d900
Jan Buchmaier <janbuchmaier@seznam.cz>
2019-09-05 15:20:53 +0200

    Added Python and Java regex

--- app/constants.py
+++ app/constants.py
@@ -1,4 +1,5 @@
 from ErrorClass import *
 
-IDENTIFICATORS = [["Traceback", 0], ["Exception", 0]]
 DICTIONARY = [Python_error, Java_error, Node_JS_error, C_error, C_sharp_error, C_plus_plus_error]
+patterns = {"File \"[a-zA-Z./]+\", line [0-9]+, in ": Python_error,
+           "\tat [a-zA-Z./]+\([A-Za-z ]+\)": Java_error}
