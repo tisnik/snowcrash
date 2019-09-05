aa95dc6b556b33a91663ddd13b666a7a69479972
AlbatrosCZ <dan-pavlik@seznam.cz>
2019-09-05 12:05:57 +0200

    add variable log

--- app/ErrorClass.py
+++ app/ErrorClass.py
@@ -5,36 +5,50 @@ Errors, Python_error, Java_error, Node_JS_error, C_error, C_sharp_error, C_plus_
 class Error:
     """This is the class for sorting each type of errors\n
     Line is a pozition of error"""
-    def __init__(self,line):
+    def __init__(self,log,line):
         self.line=line
+        self.log=log
+
+
+
 
 class Python_error(Error):
     """This is the class for sorting Python errors\n
     Line is a pozition of error"""
-    def __init__(self,line):
-        self.error=Error(line)
+    def __init__(self,log,line):
+        self.error=Error(log,line)
+
+
 class Java_error(Error):
     """This is the class for sorting Java errors\n
     Line is a pozition of error"""
-    def __init__(self,line):
-        self.error=Error(line)
+    def __init__(self,log,line):
+        self.error=Error(log,line)
+
+
 class C_error(Error):
     """This is the class for sorting C errors\n
     Line is a pozition of error"""
-    def __init__(self,line):
-        self.error=Error(line)
+    def __init__(self,log,line):
+        self.error=Error(log,line)
+
+
 class C_sharp_error(Error):
     """This is the class for sorting C# errors\n
     Line is a pozition of error"""
-    def __init__(self,line):
-        self.error=Error(line)
+    def __init__(self,log,line):
+        self.error=Error(log,line)
+
+
 class C_plus_plus_error(Error):
     """This is the class for sorting C++ errors\n
     Line is a pozition of error"""
-    def __init__(self,line):
-        self.error=Error(line)
+    def __init__(self,log,line):
+        self.error=Error(log,line)
+
+        
 class Node_JS_error(Error):
     """This is the class for sorting JavaScript errors\n
     Line is a pozition of error"""
-    def __init__(self,line):
-        self.error=Error(line)
+    def __init__(self,log,line):
+        self.error=Error(log,line)
