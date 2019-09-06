cb4efcce727632fe2a95607fd5eb3cdc2d18f0b1
Vojtěch Kudláček <kudlacek.vojtech@sspbrno.cz>
2019-09-06 10:59:33 +0200

    Bug_search changed to use regex and repaird java error regex

--- app/bug_search.py
+++ app/bug_search.py
@@ -1,15 +1,26 @@
 import constants
 import re
 
+''' Getting start line of error'''
+
 
 def get_line_of_error(log):
-    errors = constants.IDENTIFICATORS
-    for l in range(len(log)):
-        for err in errors:
-            if log.startswith(err[0]):
-                return l
+    errors = constants.patterns
+    for line_number, line in enumerate(log.split("\n"), 1):
+        for err in errors.keys():
+            if re.match(err, line):
+                return line_number - 2
+
+
+''' Separating error form log '''
 
 
 def get_error_from_log(log):
-    line = get_error_from_log(log)
-    return log.substring(line-1)
+    line = get_line_of_error(log)
+    log = log.split("\n")
+    return "\n".join(log[line:])
+
+
+if __name__ == "__main__":
+    with open("../tests/logs/java/ConnectException.log") as f:
+        print(get_error_from_log("jsah\ndjk\nash\n" + f.read()))
