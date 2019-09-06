7e374bb72fabfbbffd23f9b896b752a215a38f74
AlbatrosCZ <dan-pavlik@seznam.cz>
2019-09-06 09:07:20 +0200

    Update python error

--- app/ErrorClass.py
+++ app/ErrorClass.py
@@ -12,12 +12,45 @@ class Error:
         self.log = log
 
 
+
+
 class Python_error(Error):
     """This is the class for sorting Python errors\n
     Line is a pozition of error"""
+    def __init__(self,log,line):
+        self.error=Error(log,line)
+        super().__init__(log,line)
+        self.remake_log()
+        self.control_line()
+        print(self.line)
 
-    def __init__(self, log, line):
-        self.error = Error(log, line)
+    def remake_log(self):
+        self.add_error_type(self.log[len(self.log)-1])
+
+    def add_error_type(self,last):
+        stop=False
+        self.error_type=""
+        for char in last:
+            if char==":":
+                stop=True
+            elif stop==False:
+                self.error_type+=char
+
+    def control_line(self):
+        for row in self.log:
+            if "File \"" in row and ", line " in row:
+                loaded=False
+                number=""
+                for index in range(len(row)):
+                    if row[index]==",":
+                        loaded=False
+                    if loaded==True and row[index]!=" ":
+                        number+=row[index]
+                    if row[index]=="e":
+                        if row[index-3:index+1]=="line":
+                            loaded=True
+        if self.line!=int(number):
+            self.line=int(number)
 
 
 class Java_error(Error):
