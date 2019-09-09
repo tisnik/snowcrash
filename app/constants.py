eff724a8798866e47446defea857d9a868162e6b
Vojtěch Kudláček <kudlacek.vojtech@sspbrno.cz>
2019-09-09 16:03:32 +0200

    Rewrite of sql_database

--- app/constants.py
+++ app/constants.py
@@ -3,3 +3,4 @@ from app.ErrorClass import *
 DICTIONARY = [Python_error, Java_error, Node_JS_error, C_error, C_sharp_error, C_plus_plus_error]
 patterns = {r"[ ]*File \"[ -ž]*\", line [0-9]+": Python_error,
             r"\tat [a-zA-Z.\/]+\([A-Za-z:.-_0-9 ]+\)": Java_error}
+database_create_script = "../tests/SQL/SQL_Create_Database_Query.sql"
--- app/sql_database.py
+++ app/sql_database.py
@@ -1,20 +1,41 @@
 from sqlite3 import *
-try:
-    open("memory.db")
-except:
-    open("memory.db","a")
-conn=connect("memory.db")
-conn.isolation_level=None
-key=conn.cursor()
-try:
-    SQL_create_query=open("../tests/SQL/SQL_Create_Database_Query.txt")
-    sql_query=SQL_create_query.read().split("\n")
-    for i in sql_query:
-        print(i)
-        conn.execute(i)
-except:
-    print("Database already exists")
+import os
+import sys
+from app import constants
 
-def add_language(language,regex):
-    global conn
-    conn.execute("INSERT INTO Language (COUNT, Language, Regex) VALUES(0,"+str(language)+","+str(regex)+");")
+
+class Sql_database:
+
+    def __init__(self, db_path="memory.db"):
+        self.conn = None
+        self.db_path = db_path
+        self.init_db()
+
+    def init_db(self):
+        if os.path.isfile(self.db_path):
+            try:
+                open(self.db_path, "a")
+            except PermissionError as err:
+                print(err)
+                sys.exit(1)
+
+        self.conn = connect(self.db_path)
+        self.conn.isolation_level = None
+        key = self.conn.cursor()
+        try:
+            create_sql = open(constants.database_create_script)
+            query = create_sql.read().split("\n")
+            for row in query:
+                self.execute(row)
+        except Error:
+            print("Database already exists")
+
+    def execute(self, sql):
+        self.conn.execute(sql)
+
+    def query(self, sql):
+        return self.conn.query(sql)
+
+    def add_language(self, language, regex):
+        self.execute(
+            "INSERT INTO Language (COUNT, Language, Regex) VALUES(0,{},{});".format(str(language), regex))
