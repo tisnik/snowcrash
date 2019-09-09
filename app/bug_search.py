43a13d57500732f796bafcc27dd97121354cf2e7
Vojtěch Kudláček <kudlacek.vojtech@sspbrno.cz>
2019-09-09 10:02:37 +0200

    Repaird bug_search and Error_class

--- app/bug_search.py
+++ app/bug_search.py
@@ -10,18 +10,18 @@ def get_line_of_error(log):
     for line_number, line in enumerate(log.split("\n"), 1):
         for err in errors.keys():
             if re.match(err, line):
-                if not (line[0] == " ") and errors[err] is Python_error:
-                    return line_number - 2
-                else:
-                    return line_number - 1
+                return line_number - 2, errors.get(err) == Python_error and line[0] == 'F'
 
 
 ''' Separating error form log '''
 
 
 def get_error_from_log(log):
-    line = get_line_of_error(log)
+    line, is_first = get_line_of_error(log)
     log = log.split("\n")
+    if is_first:
+        log.insert(0, " ")
+        line += 1
     return log[line:-1]
 
 
