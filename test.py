8f4dbe29aa529262ca24b271d3794c925c82614e
Vojtěch Kudláček <kudlacek.vojtech@sspbrno.cz>
2019-09-10 10:43:08 +0200

    In test added test errors to database

--- test.py
+++ test.py
@@ -1,18 +1,23 @@
 from app import ErrorClass
 from app import bug_search
 from app import language_identity
+from app.sql_database import *
 
 import os
+
+sql_database = Sql_database()
 
 errors=[]
 for root, dirs, files in os.walk("./tests/logs/python"):
     for filename in files:
         errors.append(open("./tests/logs/python/"+filename))
-for root, dirs, files in os.walk("../tests/logs/java"):
+for root, dirs, files in os.walk("./tests/logs/java"):
     for filename in files:
         errors.append(open("./tests/logs/java/"+filename))
 for i in range(len(errors)):
     errors[i]=bug_search.get_error_from_log(errors[i].read())
 for i in range(len(errors)):
     errors[i] = language_identity.identify(errors[i])
+
+    sql_database.add_to_table("Errors", [type(errors[i]).__name__, errors[i].error_type, errors[i].path, errors[i].line, errors[i].error_msg])
     print(errors[i])
