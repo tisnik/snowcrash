f0dfbc7f47078ad4d4210bc3586485b176537f7e
AlbatrosCZ <dan-pavlik@seznam.cz>
2019-09-10 11:25:30 +0200

    add function to write

--- app/sql_database.py
+++ app/sql_database.py
@@ -38,11 +38,11 @@ class Sql_database:
 
     def add_to_table(self,table,variables=[]):
         """
-table is name of table to write. It can take values (Error, Type, Language, Solution)
+table is name of table to write. It can take values (Errors, Type, Language, Solution)
     
-variables is list of variables to tables. It can take values listed below.
+variables is list of variables to tables. It can take values listed below. Variables must be defined, even those that are optional. If you don't want to set a variable, enter it as False.
     
-Table Error has: 
+Table Errors has: 
     Language - It's a File Language (VARCHAR NOT NULL)
     Type - It's a Error type's name (VARCHAR NOT NULL)
     Path - It's a Fill Path of error (VARCHAR NULL) 
@@ -61,22 +61,73 @@ Table Language has:
     Regex - It's a string to recognition File (VARCHAR NOT NULL)
     
 Table Solution has: 
-    Type - It's a Error type's name (VARCHAR NOT NULL)
     Language - It's a File Language (VARCHAR NOT NULL)
+    Type - It's a Error type's name (VARCHAR NOT NULL)
     Priority - It's a Priority to use Soulution, it's 1(First Use) to 999(Last Use) (INT NOT NULL)      
     Solution - It's a Solution of error (VARCHAR NOT NULL)
         """
-        tables={'Error':['SELECT TypeID FROM Type WHERE Type=='+str(variables[1])+' and Language =='+str(variables[0]),
-                         'INSERT INTO Error'],
+        tables={'Errors':['SELECT TypeID FROM Type WHERE Type=='+str(variables[1])+' AND Language =='+str(variables[0]),
+                         'INSERT INTO Errors'],
                 'Type':['INSERT INTO Type'],
                 'Language':['INSERT INTO Language'],
-                'Solution':['SELECT TypeID FROM Type WHERE Type=='+str(variables[1])+' and Language =='+str(variables[0]),
+                'Solution':['SELECT TypeID FROM Type WHERE Type=='+str(variables[1])+' AND Language =='+str(variables[0]),
                             'INSERT INTO Solution']}
-        if table in ['Error','Type','Language','Solution']:
+        values={'Errors':["Path", "Line", "MSG", "First", "Last", "TypeID"],
+                'Type':["Language", "TypeName", "MSG"],
+                'Language':["Language", "Regex"],
+                'Solution':["Priority", "Soulution", "TypeID"]}
+        if table in ['Errors','Type','Language','Solution']:
             for query in tables[str(table)]:
                 if query[0:5]=="SELECT":
-                    self.key.execute(query)
-                    get=self.key.fetchall()
-                    print(get)
+                    try:
+                        self.key.execute(query)
+                        variables.append(self.key.fetchall()[0])
+                    except:
+                        if variables[0] not in self.key.fetchall():
+                            self.add_to_table("Language",[variables[0],input("add regex to language "+str(variables[0]))+":"])
+                        self.add_to_table("Type",[variables[0],variables[1],False])
+                        self.key.execute("SELECT Language FROM Language")
+                        self.key.execute(query)
+                        variables.append(self.key.fetchall()[0])
+                elif query[0:5]=="INSERT" and table in ["Errors","Solution"]:
+                    for i in range(len(values[table])):
+                        if variables[i+2]!=False:
+                            query+=values[table][i]+","
+                    if table=="Errors":
+                        query+="ErrorID,COUNT) VALUES("
+                    else:
+                        query+="SolutionID,COUNT) VALUES("
+                    for i in range(len(values[table])):
+                        if variables[i+2]!=False:
+                            query+=str(variables[i+2])+","
+                    if table=="Errors":
+                        query+=str(int(self.key.execute("SELECT max(ErrorID)FROM Errors").fetchall()[0])+1)+",0)"
+                    else:
+                        query+=str(int(self.key.execute("SELECT max(SolutionID)FROM Solution").fetchall()[0])+1)+",0)"
+                    try:
+                        self.conn.execute(query)
+                        return True
+                    except:
+                        return False
+                elif query[0:5]=="INSERT" and table in ['Type','Language']:
+                    for i in range(len(values[table])):
+                        if variables[i]!=False:
+                            query+=values[table][i]+","
+                    if table=="Type":
+                        query+="TypeID,COUNT) VALUES("
+                    else:
+                        query+="COUNT) VALUES("
+                    for i in range(len(values[table])):
+                        if variables[i]!=False:
+                            query+=str(variables[i])+","
+                    if table=="Type":
+                        query+=str(int(self.key.execute("SELECT max(TypeID)FROM Type").fetchall()[0])+1)+",0)"
+                    else:
+                        query+="0)"
+                    try:
+                        self.conn.execute(query)
+                        return True
+                    except:
+                        return False
         else:
-            return "This table isn't Exist"
+            return False
