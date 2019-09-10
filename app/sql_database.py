a1966c335705c08af090aceae3135f9599a1292a
AlbatrosCZ <dan-pavlik@seznam.cz>
2019-09-10 12:55:38 +0200

    function add_to_table()

--- app/sql_database.py
+++ app/sql_database.py
@@ -89,11 +89,34 @@ class Sql_database:
 
     def add_to_table(self, table, variables=[]) -> bool:
         """
-        the table is the name of the table to write. It can take values (Errors, Type, Language, Solution)
-        variables are a list of variables to tables. It can take the values listed below.
-        Variables must be defined, even those that are optional.
-        If you don't want to set a variable, enter it as False.
-        """ 
+table is name of table to write. It can take values (Errors, Type, Language, Solution)
+    
+variables is list of variables to tables. It can take values listed below. Variables must be defined, even those that are optional. If you don't want to set a variable, enter it as False.
+    
+Table Errors has: 
+    Language - It's a File Language (VARCHAR NOT NULL)
+    Type - It's a Error type's name (VARCHAR NOT NULL)
+    Path - It's a Fill Path of error (VARCHAR NULL) 
+    Line - It's a Line of error (INT NULL)
+    MSG - It's a Message of error (VARCHAR NULL)
+    First - It's a First time of error record (YYYY-MM-DD HH-MM-SS NULL) 
+    Last - It's a Last time of error record (YYYY-MM-DD HH-MM-SS NULL) 
+    
+Table Type has:
+    Language - It's a Error Language (VARCHAR NOT NULL)
+    TypeName - It's a Error type (VARCHAR NOT NULL)
+    MSG - It's a Moustly use message (VARCHAR) NULL
+    
+Table Language has:
+    Language - It's a Name of programing language (VARCHAR NOT NULL)
+    Regex - It's a string to recognition File (VARCHAR NOT NULL)
+    
+Table Solution has: 
+    Language - It's a File Language (VARCHAR NOT NULL)
+    Type - It's a Error type's name (VARCHAR NOT NULL)
+    Priority - It's a Priority to use Soulution, it's 1(First Use) to 999(Last Use) (INT NOT NULL)      
+    Solution - It's a Solution of error (VARCHAR NOT NULL)
+        """
         tables={'Errors':['SELECT TypeID FROM Type WHERE TypeName=\"'+str(variables[1])+'\" AND Language=\"'+str(variables[0])+'\"',
                          'INSERT INTO Errors('],
                 'Type':['INSERT INTO Type('],
