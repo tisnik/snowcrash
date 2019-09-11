8e81a5636ee0f22ff94e464c5c1b0830187991cb
Vojtěch Kudláček <kudlacek.vojtech@sspbrno.cz>
2019-09-11 12:20:35 +0200

    Updated main.py

--- main.py
+++ main.py
@@ -2,32 +2,55 @@
 import click
 from app import bug_search
 from app import language_identity
+from app.sql_database import Sql_database
 
 
-def look_in_db(filename):
-    pass
+def show_db():
+    """Printout all errors in DB"""
+    db = Sql_database()
+    errors = db.to_errors()
+    for error in errors:
+        print(error)
 
 
-def process_log(log):
+def get_processed_log(log):
+    """Processing log to object"""
     clear_log = bug_search.get_error_from_log(log)
-    error = language_identity.identify(clear_log)
-    print(error)
+    return language_identity.identify(clear_log)
+
+
+def print_log(log):
+    """Printing processed log"""
+    print(get_processed_log(log))
 
 
 def load_from_file(filename):
+    """Loading log from file"""
+    data = ""
     with open(filename) as f:
-        process_log(f.read())
+        data = f.read()
         f.close()
+    return data
+
+
+def process_log(log):
+    """Processing log and adding to the DB"""
+    error = get_processed_log(log)
+    db = Sql_database()
+    success = db.add_Error(error)
+    print(str(error) + ": ", {True: "Success", False: "Failed"}[success])
 
 
 @click.command()
-@click.option("--filename", type=click.Path(exists=True), help="Filename of the log to read from")
-@click.option("--mode", type=click.Choice(['print', 'lookInDb']))
+@click.option("-f", "--filename", type=click.Path(exists=True), help="Filename of the log to read from")
+@click.option("-m", "--mode", required=True, type=click.Choice(['process', 'show_db', 'print']))
 def main(filename, mode):
     if mode == 'print':
-        load_from_file(filename)
-    elif mode == 'lookInDb':
-        look_in_db(filename)
+        print_log(load_from_file(filename))
+    elif mode == 'show_db':
+        show_db()
+    elif mode == 'process':
+        process_log(load_from_file(filename))
 
 
 if __name__ == "__main__":
