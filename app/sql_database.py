9c2d3b541692e46edb947900365892bd42af72b6
AlbatrosCZ <dan-pavlik@seznam.cz>
2019-09-12 13:14:12 +0200

    Reedit function and daatabase

--- app/sql_database.py
+++ app/sql_database.py
@@ -220,9 +220,9 @@ class Sql_database:
         insert = "INSERT INTO {}({}) VALUES(\'{}\');"
         values = {'Error': {"Path": None, "Line": None, "MSG": None,
                             "First": None, "Last": None, "TypeID": None},
-                  'Type': {"TypeName": None, "MSG": None, "LanguageID": None},
-                  'Language': {"Language": None, "Regex": None, "Version": None},
-                  'Solution': {"Solution": None, "Priority": None, "TypeID": None}}
+                'Type': {"LanguageID": None, "TypeName": None, "MSG": None},
+                'Language': {"Language": None, "Regex": None, "Version": None},
+                'Solution': {"Solution": None, "Priority": None, "TypeID": None}}
         if table in values.keys():
             value = self.parse_table(values[table], variables)
             if table == "Solution":
