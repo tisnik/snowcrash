c6ad69343f62eb8074364d0279eb765e9165dc34
vojtakudlacek <vojtakudlacek@centrum.cz>
2019-09-16 14:35:13 +0200

    Added Help_Dialog class

---
+++ app/help.py
@@ -0,0 +1,32 @@
+from tkinter import *
+
+
+class Help_Dialog:
+
+    def __init__(self, data_file):
+        self.window = Tk()
+        self.file_text = ""
+        self.load_text(data_file)
+        self.add_components()
+        self.window.mainloop()
+
+    def load_text(self, file):
+        with open(file, mode="r") as f:
+            self.file_text = f.read()
+            f.close()
+
+    def add_components(self):
+        scrollbar = Scrollbar(self.window)
+        scrollbar.pack(side=RIGHT, fill=Y)
+
+        label = Text(self.window)
+        label.insert(END, str(self.file_text))
+        label.config(width=75, height=15)
+        label.config(yscrollcommand=scrollbar.set)
+        label.pack(fill="both")
+        Button(self.window, text="Ok", command=lambda: self.on_ok()).pack(fill="both")
+        scrollbar.config(command=label.yview)
+        label.config(xscrollcommand=scrollbar.set)
+
+    def on_ok(self):
+        self.window.quit()
